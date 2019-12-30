a0813c66fb43da4efd9ee6415dc1109d85d14a7c
Han Wang <cowboybebophan@gmail.com>
2019-12-30 16:26:55 -0500

    Solved problems

---
+++ src/com/ascending/problems/_0236_LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,28 @@
+package com.ascending.problems;
+
+public class _0236_LowestCommonAncestorOfABinaryTree {
+
+     // Definition for a binary tree node.
+
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int val) {
+             this.val = val;
+         }
+     }
+
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null || root == p || root == q) return root;
+
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+            if (left != null && right != null) return root;
+            return left != null ? left : right;
+        }
+    }
+
+}
